50e0bdf1967047ef63d73a409d29984eec6ae969
Rajendra Gosavi <36725053+rajendragosavi@users.noreply.github.com>
2019-05-08 18:04:23 +0530

    Create sql_schema.sql

---
+++ sql_schema.sql
@@ -0,0 +1,28 @@
+CREATE TABLE users(
+    username VARCHAR (255)     NOT NULL,      
+    password VARCHAR (255)   NOT NULL
+    );
+    
+    
+    CREATE TABLE CUSTOMERS(
+    ID   INT              NOT NULL,
+    LastName VARCHAR (20)     NOT NULL,
+    FirstName VARCHAR(255)              NOT NULL,
+    State  VArCHAR (255) ,
+    City   VARCHAR (255),       
+        PRIMARY KEY (ID)
+    );
+
+
+
+ CREATE TABLE VOTERS(
+    VoterID   INT              NOT NULL,
+    LastName VARCHAR (20)     NOT NULL,
+    FirstName VARCHAR(255)              NOT NULL,
+    State  VArCHAR (255) ,
+    City   VARCHAR (255),       
+        PRIMARY KEY (VoterID)
+    );
+
+    
+    
